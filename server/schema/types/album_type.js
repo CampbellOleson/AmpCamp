0167f34275660375363378cd831cb64de140ad91
Ken Klooster <kklooster3910@gmail.com>
2019-08-31 12:48:18 -0400

    commit fresh master

--- server/schema/types/album_type.js
+++ server/schema/types/album_type.js
@@ -6,31 +6,32 @@ const UserType = require("./user_type");
 const Song = mongoose.model("songs");
 const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLList } = graphql;
 
-// const AlbumType = new GraphQLObjectType({
-//   name: "AlbumType",
-//   fields: () => ({
-//     _id: { type: GraphQLID },
-//     title: { type: GraphQLString },
-//     description: { type: GraphQLString },
-//     by: { type: GraphQLString },
-//     coverPhotoUrl: { type: GraphQLString },
-//     songs: {
-//       type: new GraphQLList(require("./song_type")),
-//       resolve(parentValue) {
-//         return Album.findById(parentValue._id)
-//           .populate("songs")
-//           .then(album => album.songs);
-//       }
-//     },
-//     artist: {
-//       type: { UserType },
-//       resolve(parentValue) {
-//         return User.findById(parentValue.artist)
-//           .then(artist => artist)
-//           .catch(error => error);
-//       }
-//     }
-//   })
-// });
+const AlbumType = new GraphQLObjectType({
+  name: "AlbumType",
+  fields: () => ({
+    _id: { type: GraphQLID },
+    title: { type: GraphQLString },
+    description: { type: GraphQLString },
+    by: { type: GraphQLString },
+    coverPhotoUrl: { type: GraphQLString },
+    songs: {
+      type: new GraphQLList(require("./song_type")),
+      resolve(parentValue) {
+          return Album.allSongs
+        // return Album.findById(parentValue._id)
+        //   .populate("songs")
+        //   .then(album => album.songs);
+      }
+    },
+    artist: {
+      type: { UserType },
+      resolve(parentValue) {
+        return User.findById(parentValue.artist)
+          .then(artist => artist)
+          .catch(error => error);
+      }
+    }
+  })
+});
 
-// module.exports = AlbumType;
+module.exports = AlbumType;
--- server/schema/types/song_type.js
+++ server/schema/types/song_type.js
@@ -5,30 +5,31 @@ const User = mongoose.model("users");
 const UserType = require("./user_type");
 const AlbumType = require("./album_type");
 const { GraphQLObjectType, GraphQLString, GraphQLID } = graphql;
+// const Album = mongoose.model('albums')
 
-// const SongType = new GraphQLObjectType({
-//   name: "SongType",
-//   fields: () => ({
-//     _id: { type: GraphQLID },
-//     title: { type: GraphQLString },
-//     audioUrl: { type: GraphQLString },
-//     album: {
-//       type: { AlbumType },
-//       resolve(parentValue) {
-//         return Album.findById(parentValue.album)
-//           .then(album => album)
-//           .catch(error => error);
-//       }
-//     },
-//     artist: {
-//       type: { UserType },
-//       resolve(parentValue) {
-//         return User.findById(parentValue.artist)
-//           .then(artist => artist)
-//           .catch(error => error);
-//       }
-//     }
-//   })
-// });
+const SongType = new GraphQLObjectType({
+  name: "SongType",
+  fields: () => ({
+    _id: { type: GraphQLID },
+    title: { type: GraphQLString },
+    audioUrl: { type: GraphQLString },
+    album: {
+      type: { AlbumType },
+      resolve(parentValue) {
+        return Album.findById(parentValue.album)
+          .then(album => album)
+          .catch(error => error);
+      }
+    },
+    artist: {
+      type: { UserType },
+      resolve(parentValue) {
+        return User.findById(parentValue.artist)
+          .then(artist => artist)
+          .catch(error => error);
+      }
+    }
+  })
+});
 
-// module.exports = SongType;
+module.exports = SongType;
