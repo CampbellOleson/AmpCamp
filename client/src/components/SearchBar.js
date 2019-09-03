b6a0516756016211d65d2266d2ac33bc35aac000
Arshia <youthinkasido@gmail.com>
2019-09-03 18:58:31 -0400

    search bar autocomplete implemented

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,54 +1,86 @@
 import React from 'react'
-
 import { Query } from "react-apollo";
 import Queries from '../graphql/queries'
 import Autosuggest from 'react-autosuggest'
+import './SearchBar.css'
 const { FETCH_ALBUMS } = Queries
 
 class SearchBar extends React.Component {
     constructor(props) {
         super(props)
-        this.filterResults = this.filterResults.bind(this)
+
+        this.items = [
+            'Eat ass volume 1',
+            'album 2',
+            'album 3',
+            'album 4',
+            'album 5',
+            'Zebra adventures of Sally Paste',
+            'Back to the thunk',
+            'action creator, shmaction greator'
+        ]
+        this.state = {
+            suggestions: [],
+
+        }
+
+        this.renderSuggestions = this.renderSuggestions.bind(this)
     }
 
-    componentDidMount() {
+    onTextChanged = (e) => {
 
+        const value = e.target.value; // current value of search bar
+        let suggestions = [];
+        let val;
+
+        if (value.length > 0) { // if there are letters in search bar...
+            const regex = new RegExp(`^${value}`, 'i'); // ?
+
+            suggestions = this.items.sort().filter(v => regex.test(v));
+
+        }
+        this.setState(() => ({ suggestions }));
     }
 
-    filterResults() {
-
+    renderSuggestions(queryResults) {
+        const { suggestions } = queryResults;
+        if (suggestions.length === 0) {
+            return null
+        }
+        console.log(suggestions)
+        return (
+            <ul className='search-completer'>
+                {suggestions.map((item) => <li>{item}</li>)}
+            </ul>
+        )
     }
 
     render() {
-
+        let queryResult
         return (
-            <div>
-
-                <Query query={FETCH_ALBUMS}>
-                    {({ loading, errors, data }) => {
-                        console.log(data)
-
-                        return (
-                            <div className="search-completer">
-                                <input type="text" className="nav-search" placeholder="find some killer music YO"></input>
-                                <ul>
-                                    <li>Test Album by Tony Smith Mcfishumz</li>
-                                    <li>Test Album by Tony Smith Mcfishumz</li>
-                                    <li>Test Album by Tony Smith Mcfishumz</li>
-                                </ul>
-                            </div>
-
-                        )
+            <Query query={FETCH_ALBUMS}>
+                {({ loading, errors, data }) => {
 
 
-                    }
-                    }
-
-                </Query>
-
-            </div>
+                    return (
+                        queryResult = { data }
+                        console.log(queryResult)
+                        < div >
+                        <div className="search-container">
+                            <input
+                                onChange={this.onTextChanged}
+                                type="text"
+                                className="search-bar"
+                                placeholder="find sick music!" />
+                            {this.renderSuggestions(queryResult)}
+                        </div>
+                        </div>
+            )
+        }}
+            </Query>
         )
     }
+
 }
 
 export default SearchBar
