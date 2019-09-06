64d7f9a4ff8cc0fdabd242fd8245f4415390b172
Arshia <youthinkasido@gmail.com>
2019-09-06 14:16:16 -0400

    added homepage scroll gallery, about to restyle

--- client/src/components/HomePageFeature.js
+++ client/src/components/HomePageFeature.js
@@ -14,64 +14,68 @@ class HomePageFeature extends React.Component {
         this.album = null
     }
     featuredPhoto(data) {
-
+        let shuffledAlbums = data.albums.sort(() => Math.random() - 0.5)
         return (
-            this.featuredAlbum = data.albums[Math.floor(Math.random() * data.albums.length)]
+            this.featuredAlbums = shuffledAlbums.slice(0, 4)
+
         )
+
     }
 
+    componentDidMount() {
+        let photoReel = document.addEventListener('DOMContentLoaded', function () {
+            let photoReel = document.getElementsByClassName('photo-reel-image')
+            debugger
+        })
+        if (photoReel) photoReel.addEventListener('mouseover', function () {
+            console.log('hovered over photo!')
+        })
+    }
+
+
     render() {
-        return (
-            <Query query={FETCH_ALBUMS_AND_ARTISTS}>
-                {({ loading, errors, data }) => {
-                    if (loading) return (
-                        <div className="lds-roller">
-                            <div></div>
-                            <div></div>
-                            <div></div>
-                            <div></div>
-                            <div></div>
-                            <div></div>
-                            <div></div>
-                            <div></div>
-                        </div>
-                    );
 
-                    if (data) {
-                    
-                        this.featuredPhoto(data)
-                        return (
-                            <div>
-                                <div className="featured-master-container">
-                                    <div className="featured-container">
-                                        <div className="featured-photo">
-                                            <Link to={`/album/${this.featuredAlbum._id}`}><img id="test" src={this.featuredAlbum.coverPhotoUrl} /></Link>
-                                        </div>
-                                        <div className="featured-info">
-                                            <h3>{this.featuredAlbum.title}</h3>
-                                            {/* <h1>{this.featuredAlbum.artist}</h1> */}
-                                            <h2>FEATURED ALBUM</h2>
-                                        </div>
 
-                                            {/* <h3>by {this.featuredAlbum.artist}</h3> */}
-                                        <div className="featured-blurb">
-                                            <h3>{this.featuredAlbum.by}</h3>
-                                            <h2>{this.featuredAlbum.title}</h2>
-                                            <h3>has definetly been the greatest album I've hear all year
-                                                its got kick, punch and just the right amount of spice.
-                                            </h3>
-                                        </div>
-
-                                    </div>
-                                </div>
-                                <FeaturedArtists data={data} />
+            return(
+            <Query query = { FETCH_ALBUMS_AND_ARTISTS } >
+                    {({ loading, errors, data }) => {
+                        if (loading) return (
+                            <div className="lds-roller">
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
                             </div>
-                        )
-                    } else {
-                        return null;
-                    }
-                }}
-            </Query>
+                        );
+
+        if (data) {
+            this.featuredPhoto(data)
+            return (
+                <div>
+                    <h1 id="daily-picks-heading">Amp Camp Daily Picks</h1>
+                    <div id="daily-picks-container">
+                        <div className="photobanner">
+                            <Link to={`/album/${this.featuredAlbums[0]._id}`}><img className="photo-reel-image" className="first" src={this.featuredAlbums[0].coverPhotoUrl} /></Link>
+                            <Link to={`/album/${this.featuredAlbums[1]._id}`}><img className="photo-reel-image" src={this.featuredAlbums[1].coverPhotoUrl} /></Link>
+                            <Link to={`/album/${this.featuredAlbums[2]._id}`}><img className="photo-reel-image" src={this.featuredAlbums[2].coverPhotoUrl} /></Link>
+                            <Link to={`/album/${this.featuredAlbums[3]._id}`}><img className="photo-reel-image" src={this.featuredAlbums[3].coverPhotoUrl} /></Link>
+                            <Link to={`/album/${this.featuredAlbums[1]._id}`}><img className="photo-reel-image" src={this.featuredAlbums[1].coverPhotoUrl} /></Link>
+
+                        </div>
+                    </div>
+                    <FeaturedArtists data={data} />
+                </div>
+            )
+        } else {
+            return null;
+        }
+    }
+}
+            </Query >
         )
     }
 
