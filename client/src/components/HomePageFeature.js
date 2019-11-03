a3a4d69ff5fe8db2eace9c160442d176def5ce46
Arshia Khoshnood <youthinkasido@gmail.com>
2019-11-03 12:36:41 -0500

    Complete initial version of new featured album page

--- client/src/components/HomePageFeature.js
+++ client/src/components/HomePageFeature.js
@@ -1,117 +1,119 @@
-import React from 'react';
-import './HomePageFeature.css';
-import FeaturedArtists from './FeaturedArtists';
-import {Query} from 'react-apollo';
-import Queries from '../graphql/queries';
-import './FeaturedArtists.css';
-import {Link} from 'react-router-dom';
-const {FETCH_ALBUMS_AND_ARTISTS} = Queries;
-
+import React from "react";
+import "./HomePageFeature.css";
+import FeaturedArtists from "./FeaturedArtists";
+import { Query } from "react-apollo";
+import Queries from "../graphql/queries";
+import "./FeaturedArtists.css";
+import { Link } from "react-router-dom";
+const { FETCH_ALBUMS_AND_ARTISTS } = Queries;
 class HomePageFeature extends React.Component {
-  constructor (props) {
-    super (props);
+  constructor(props) {
+    super(props);
     this.photo = null;
     this.album = null;
+    this.state = {
+      currentPhoto: 1,
+
+    }
+    this.shuffledPhotos = false;
+
   }
-  featuredPhoto (data) {
-    let shuffledAlbums = data.albums.sort (() => Math.random () - 0.5);
-    return (this.featuredAlbums = shuffledAlbums.slice (0, 6));
+  featuredPhoto(data) {
+    let shuffledAlbums = data.albums.sort((a, b) => a > b);
+    // let shuffledAlbums = data.albums.sort(() => Math.random() - 0.5);
+    return (this.featuredAlbums = shuffledAlbums.slice(0))
+  }
+  componentDidMount() {
+    // let photoReel = document.addEventListener("DOMContentLoaded", function () {
+    // });
+    // if (photoReel)
+    //   photoReel.addEventListener("mouseover", function () {
+    //     console.log("hovered over photo!");
+    //   });
   }
 
-  componentDidMount () {
-    let photoReel = document.addEventListener (
-      'DOMContentLoaded',
-      function () {}
-    );
-    if (photoReel)
-      photoReel.addEventListener ('mouseover', function () {
-        console.log ('hovered over photo!');
-      });
+  nextPhoto(increment, e) {
+    console.log('posOrNeg', increment)
+    e.preventDefault()
+    let newState = {};
+
+    if (increment === 1 && this.state.currentPhoto < this.featuredAlbums.length) {
+      this.state.currentPhoto++;
+      console.log('album length', this.featuredAlbums.length)
+      increment = 0;
+    }
+    if (increment === 1 && this.state.currentPhoto === this.featuredAlbums.length) {
+      this.state.currentPhoto = 0
+      console.log('album length', this.featuredAlbums.length)
+
+      increment = 0;
+    }
+    if (increment === -1 && this.state.currentPhoto > 0) {
+      this.state.currentPhoto--;
+      increment = 0;
+    }
+
+    if (increment === -1 && this.state.currentPhoto === 0) {
+      this.state.currentPhoto = this.featuredAlbums.length
+      increment = 0;
+    }
+
+
+    newState.currentPhoto = this.state.currentPhoto
+
+    console.log(newState)
+    this.setState(newState)
+    console.log('state', this.state)
+
   }
 
-  render () {
+
+
+
+  render() {
+
     return (
       <Query query={FETCH_ALBUMS_AND_ARTISTS}>
-        {({loading, errors, data}) => {
+        {({ loading, errors, data }) => {
           if (loading)
             return (
               <div className="lds-roller">
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
               </div>
             );
-
           if (data.albums.length > 0) {
-            this.featuredPhoto (data);
+            this.shuffledPhotos = !this.shuffledPhotos
+
+            this.featuredPhoto(data);
+
+            this.photosToCycle = this.featuredAlbums.map(album => {
+              return <Link to={`/album/${album._id}`}>
+                <img alt="wutang"
+                  className="photo-reel-image"
+                  src={album.coverPhotoUrl}
+                ></img>
+              </Link>
+            })
+
             return (
               <div>
                 <h1 id="daily-picks-heading">Amp Camp Daily Picks</h1>
-                <div className="w3-content w3-display-container">
+                <div id="daily-picks-container">
+                  <div className="photobanner">
+                    <button className="left-photo" onClick={(e) => this.nextPhoto(-1, e)}></button>
+                    {this.photosToCycle[this.state.currentPhoto !== 0 ? this.state.currentPhoto - 1 : 1]}
+                    <button className="right-photo" onClick={(e) => this.nextPhoto(1, e)}></button>
 
-                  <Link to={`/album/${this.featuredAlbums[0]._id}`}>
-                    <img
-                      alt="wutang"
-                      className="mySlides"
-                      src={this.featuredAlbums[0].coverPhotoUrl}
-                    />
-                  </Link>
-                  <Link to={`/album/${this.featuredAlbums[1]._id}`}>
-                    <img
-                      alt="wutang"
-                      className="mySlides"
-                      src={this.featuredAlbums[1].coverPhotoUrl}
-                    />
-                  </Link>
-                  <Link to={`/album/${this.featuredAlbums[2]._id}`}>
-                    <img
-                      alt="wutang"
-                      className="mySlides"
-                      src={this.featuredAlbums[2].coverPhotoUrl}
-                    />
-                  </Link>
-                  <Link to={`/album/${this.featuredAlbums[3]._id}`}>
-                    <img
-                      alt="wutang"
-                      className="mySlides"
-                      src={this.featuredAlbums[3].coverPhotoUrl}
-                    />
-                  </Link>
-
-                  <div className="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle">
-
-                    <div
-                      class="w3-left w3-hover-text-khaki"
-                      onclick="plusDivs(-1)"
-                    >
-                      &#10094;
-                    </div>
-                    <div
-                      class="w3-right w3-hover-text-khaki"
-                      onclick="plusDivs(1)"
-                    >
-                      &#10095;
-                    </div>
-                    <span
-                      class="w3-badge demo w3-border w3-transparent w3-hover-white"
-                      onclick="currentDiv(1)"
-                    />
-                    <span
-                      class="w3-badge demo w3-border w3-transparent w3-hover-white"
-                      onclick="currentDiv(2)"
-                    />
-                    <span
-                      class="w3-badge demo w3-border w3-transparent w3-hover-white"
-                      onclick="currentDiv(3)"
-                    />
                   </div>
                 </div>
-                {/* <h1>Featured Artists</h1> */}
+                <h1>Featured Artists</h1>
                 <FeaturedArtists data={data} />
               </div>
             );
@@ -123,5 +125,4 @@ class HomePageFeature extends React.Component {
     );
   }
 }
-
 export default HomePageFeature;
